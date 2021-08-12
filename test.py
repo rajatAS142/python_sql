c1d8cf240e24b5d3cbefc705bc4712d32d9980a8
Ankit-Sigmoid <88632071+AnkitNayan11@users.noreply.github.com>
2021-08-12 18:03:53 +0530

    Update test.py
    indent was not proper in code

--- test.py
+++ test.py
@@ -3,6 +3,7 @@ import pandas as pd
 from openpyxl.workbook import Workbook
 db = psycopg2.connect(database="AssignmentPythonSQL", user="postgres", password="Rajat@98")
 
+#indent was not proper in code
 try:
     data = pd.read_sql("SELECT e.empno ,e.ename, m .ename manager FROM emp e LEFT  JOIN emp m   ON m.empno = e.mgr", con=db)  # Storing Query result in DataFrames
     data.to_excel("Output1.xlsx")  # Converting DataFrame to Excel file
